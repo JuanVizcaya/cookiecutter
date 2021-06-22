2e085490c15330c18553bad869e53263a7288859
JuanVizcaya <juanj.vizcaya@gmail.com>
2021-06-22 18:07:03 -0500

    project started

---
+++ {{cookiecutter.project_slug}}/apps/utils/mixins.py
@@ -0,0 +1,15 @@
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TimestampeMixin(models.Model):
+    """ Mixin for timestampe models """
+
+    created_at = models.DateTimeField(
+        verbose_name=_('Created at'), auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_('Updated at'),
+        auto_now=True
+    )
